5c746393fe09ecb60573fa8d81de7d891dce491e
Kawamura Shintaro <kawasin73@gmail.com>
2020-10-05 10:19:06 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 蟻本
+
+https://qiita.com/drken/items/2f56925972c1d34e05d8
+
+これをやっていく！
+
+C++ 入門 : https://atcoder.jp/contests/APG4b
---
+++ template.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALL(a)  (a).begin(),(a).end()
+typedef int long long ll;
+
+int main() {
+
+}
